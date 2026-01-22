37c9c4ea1f7b38c68cf86c111092405e632dfe8e
vodafon <vodafon.ua@gmail.com>
2026-01-22 11:38:46 +0000

    exec requests from external tools and send it throw proxy

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/vodafon/rawrequest
+
+go 1.25.5
+
+require github.com/vodafon/rawhttp v0.2.1
+
+require (
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/vodafon/vgutils v0.0.0-20201031081340-1b6be1866ddb // indirect
+	golang.org/x/net v0.49.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,48 +4,86 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
+	"net/url"
 	"os"
+
+	"github.com/vodafon/rawhttp"
 )
 
-var (
-	flagFile               = flag.String("file", "", "path to request file")
-	flagAddr               = flag.String("addr", "", "example.com:80")
-	flagTLS                = flag.Bool("tls", false, "TLS connect")
-	flagTimes              = flag.Int("times", 1, "repeat requests")
-	flagPrintResponseMatch = flag.String("response-match", "", "print responses if match")
-)
+var flagProxy = flag.String("x", "http://127.0.0.1:8080", "proxy")
+
+type Input struct {
+	URL     string
+	Request []byte
+}
 
 func main() {
 	flag.Parse()
-	if *flagFile == "" || *flagAddr == "" || *flagTimes < 1 {
-		flag.PrintDefaults()
+
+	// io.ReadAll reads until EOF (Ctrl+D or pipe close)
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read stdin: %v\n", err)
 		os.Exit(1)
 	}
 
-	bytesMatch := []byte(*flagPrintResponseMatch)
-	lMatch := len(bytesMatch)
-	client := NewDefaultClient()
-	for i := 0; i < *flagTimes; i++ {
-		req, resp := client.NewRequestResponse()
-		req.Addr = *flagAddr
-		req.IsTLS = *flagTLS
-		req.Rawdata = loadRequest(*flagFile)
-		err := client.Do(req, resp)
-		if err != nil {
-			log.Fatalf("Do request error: %v", err)
-		}
-		if lMatch > 0 && bytes.Contains(resp.Rawdata, bytesMatch) {
-			fmt.Printf("\n\n%s\n\n", resp.Rawdata)
-		}
+	input, err := ParseData(data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "invalid input: %v\n", err)
+		os.Exit(1)
 	}
+
+	client := rawhttp.NewDefaultClient()
+	defer client.Close()
+
+	if *flagProxy != "" {
+		u, err := url.Parse(*flagProxy)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to proxy URL: %v\n", err)
+			os.Exit(1)
+		}
+
+		client.SetProxy(u)
+	}
+
+	req := &rawhttp.Request{}
+	resp := &rawhttp.Response{}
+
+	req.URL = input.URL
+	req.Rawdata = input.Request
+
+	err = client.Do(req, resp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "do request error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s\n", resp.Bytes())
 }
 
-func loadRequest(filename string) []byte {
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Fatalf("Load request error: %v", err)
+func ParseData(data []byte) (*Input, error) {
+	if !bytes.HasPrefix(data, []byte("#")) {
+		return nil, fmt.Errorf("invalid input format. target line not found")
 	}
-	return file
+
+	// 1. Split on the first newline to separate the URL line from the request
+	// We use \n because even if the request uses \r\n, the first line usually
+	// ends with a simple newline in these custom formats.
+	parts := bytes.SplitN(data, []byte("\n"), 2)
+
+	if len(parts) < 2 {
+		return nil, fmt.Errorf("invalid format: could not find request body after URL line")
+	}
+
+	input := &Input{}
+
+	// 2. Extract and clean the URL (u)
+	// Remove '#' and any surrounding whitespace
+	input.URL = string(bytes.TrimSpace(bytes.TrimPrefix(parts[0], []byte("#"))))
+
+	// 3. Extract the raw request (request)
+	input.Request = parts[1]
+
+	return input, nil
 }
